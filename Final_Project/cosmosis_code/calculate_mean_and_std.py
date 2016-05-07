193fd8cc4cda25b112aabec4cf76d85de55a9dfe
Tom McClintock <tmcclintock@email.arizona.edu>
2016-05-07 10:52:36 -0700

    Added a file that calculates the mean and standard deviation of the chains.

---
+++ Final_Project/cosmosis_code/calculate_mean_and_std.py
@@ -0,0 +1,23 @@
+"""
+This script calculates the mean and standard deviations
+from the chains to report in the tables.
+"""
+
+import numpy as np
+
+name = "betoule"
+prior = "withpriors/prior"
+priorfigname = "withprior"
+modelname = "wcdm"
+
+#Get the chain
+chainpath = "chains/%s/%s_stuff/%s_astr_513_chain.txt"%(modelname,name,prior)
+chain = np.genfromtxt(chainpath)
+
+mean = np.mean(chain,0)
+std = np.std(chain,0)
+
+print mean
+print std
+
+#Edit the above to print in a format suited to the paper
